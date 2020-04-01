70f364b5267d33771dcca732d18c7ce1f4f4f00a
Ari Mouzakis <ari.mouzakis1@gmail.com>
2020-04-01 17:43:25 +1100

    Week 3 - Part 1 with the basic intent

--- app/src/main/java/com/example/kotlinnyt/DetailActivity.kt
+++ app/src/main/java/com/example/kotlinnyt/DetailActivity.kt
@@ -2,11 +2,14 @@ package com.example.kotlinnyt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
+        textview.text = intent.getStringExtra(MainActivity.MAIN_ACTIVITY_EXTRA)
     }
 }
--- app/src/main/java/com/example/kotlinnyt/MainActivity.kt
+++ app/src/main/java/com/example/kotlinnyt/MainActivity.kt
@@ -5,11 +5,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    companion object {
+        public val MAIN_ACTIVITY_EXTRA = "Article"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        article1.setOnClickListener(View.OnClickListener {
+            intent = Intent(applicationContext, DetailActivity::class.java).apply {
+                putExtra(MAIN_ACTIVITY_EXTRA, "I am displaying article 1")
+            }
+            startActivity(intent)
+        })
+
+        article2.setOnClickListener(View.OnClickListener {
+            intent = Intent(applicationContext, DetailActivity::class.java).apply {
+                putExtra(MAIN_ACTIVITY_EXTRA, "I am displaying article 2")
+            }
+            startActivity(intent)
+        })
     }
 }
